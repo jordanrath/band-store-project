aa976fb328deae416382bc8870fe86759a8175c3
jordanrath <rath.jordan@icloud.com>
2022-10-31 23:02:13 -0600

    cleaned up package.json, changed license and removed extra files

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.set('view engine', 'ejs') //makes front end use ejs to render it's views\
-app.use(express.static('Public'))
-
-app.listen(3000)
